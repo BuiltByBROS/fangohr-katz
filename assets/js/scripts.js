262978ad025e08db753a051c07c656d7a3249fd2
Chris Serron <christian@bros.me>
2017-08-05 16:13:03 -0300

    slide fixed

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -47,8 +47,8 @@ $(document).ready(function () {
             $.fn.fullpage.destroy('all');
 
             initFP('.' + topicClass);
-            $.fn.fullpage.silentMoveTo(2);
-            $.fn.fullpage.moveTo(3);
+            $.fn.fullpage.silentMoveTo(1); // With cover is 2
+            $.fn.fullpage.moveTo(2);// With cover is 3
             $('#topicClassName').val('.' + topicClass);
 
         }, 300);
